922096650edf990cc55a52657e2cc0ca34468cb1
fumi uk <dcfumiya.238@gmail.com>
2021-12-02 08:43:02 +0900

    tutorials-apis

---
+++ src/main/java/com/tutorials/tutorialsapis/TutorialsApisApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.tutorialsapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialsApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialsApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorials/tutorialsapis/TutorialsApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.tutorialsapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialsApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
